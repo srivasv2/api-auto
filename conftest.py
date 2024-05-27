5e8b15a0ae27060242a01fb2ca462106a11da082
srivasv2 <vivekbit2002@yahoo.com>
2024-05-27 00:40:07 -0500

    renamed test data folder

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ import json
 # Pytest fixture that runs once before each test in this module
 @pytest.fixture(scope='module')
 def start_exec():
-    file = open("Fixtures/test.json")
+    file = open("Test Data/test.json")
     req_json = json.loads(file.read())
     file.close()
     return req_json
